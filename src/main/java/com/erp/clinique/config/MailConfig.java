3ed29839e834b57a9a9cfadb74e4e57c5733e45a
Nekena <nekenarakotomalala760@gmail.com>
2026-05-16 03:23:55 +0300

    config mail

--- src/main/java/com/erp/clinique/config/MailConfig.java
+++ src/main/java/com/erp/clinique/config/MailConfig.java
@@ -16,7 +16,7 @@ public class MailConfig {
 	    mailSender.setHost("smtp.gmail.com");
 	    mailSender.setPort(587);
 	    mailSender.setUsername("nekenarakotomalala760@gmail.com"); 
-	    mailSender.setPassword("ykeswwizbuibeoqs");             
+	    mailSender.setPassword("srghtiftjtbnsvaa");             
 	    Properties props = mailSender.getJavaMailProperties();
 	    props.put("mail.transport.protocol", "smtp");
 	    props.put("mail.smtp.auth", "true");
